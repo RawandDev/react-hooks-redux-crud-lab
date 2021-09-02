64cb5304dc3f4532283db0af3f30c0db3fbf14be
RawandDev <rawand.200091384037@gmail.com>
2021-09-02 22:54:44 +0300

    done!

--- src/features/restaurants/RestaurantInput.js
+++ src/features/restaurants/RestaurantInput.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { restaurantAdded } from "./restaurantsSlice";
 
 function RestaurantInput() {
-  return <div>Restaurant Input</div>;
+  const [name, setName] = useState("");
+  const dispatch = useDispatch();
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    dispatch(restaurantAdded(name));
+    setName("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name
+        <input type="text" name="name" value={name} onChange={handleChange} />
+      </label>
+      <button type="submit">Add Restaurant</button>
+    </form>
+  );
 }
 
 export default RestaurantInput;
---
+++ src/features/reviews/reviewsSlice.js
@@ -0,0 +1,27 @@
+import { v4 as uuid } from "uuid";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  entities: [],
+};
+
+const reviewsSlice = createSlice({
+  name: "reviews",
+  initialState,
+  reducers: {
+    reviewAdded(state, action) {
+      state.entities.push({
+        id: uuid(),
+        ...action.payload,
+      });
+    },
+    reviewRemoved(state, action) {
+      const index = state.entities.findIndex((r) => r.id === action.payload);
+      state.entities.splice(index, 1);
+    },
+  },
+});
+
+export const { reviewAdded, reviewRemoved } = reviewsSlice.actions;
+
+export default reviewsSlice.reducer;
